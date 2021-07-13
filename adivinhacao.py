4ee6590236fead76ec9d2462f558e91cc8f06c88
Anderson F <andy.walter@hotmail.com>
2021-07-13 15:43:01 -0300

    Ajuste de código

--- adivinhacao.py
+++ adivinhacao.py
@@ -6,7 +6,7 @@ numero_secreto = 42
 tentativas = 1
 total_de_tentativas = 3
 
-for tentativas in range (1, total_de_tentativas + 1):
+for tentativas in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(tentativas, total_de_tentativas))
     chute = int(input("Chute um número entre 1 e 100: "))
     print("Você digitou: ", chute)
@@ -19,15 +19,14 @@ for tentativas in range (1, total_de_tentativas + 1):
     maior = chute > numero_secreto
     menor = chute < numero_secreto
 
-    if(acertou):
+    if acertou:
         print("Você acertou!!")
         break
 
-    elif(maior):
+    elif maior:
         print("Você errou! O número que você chutou é maior que o número secreto")
 
-    elif(menor):
+    elif menor:
         print("Você errou! O número que você chutou é menor que o número secreto")
 
 print("Fim do jogo")
-
