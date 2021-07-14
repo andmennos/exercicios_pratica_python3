e40066f24ee6b5f3f65e58064ca3622c9e014300
Anderson F <andy.walter@hotmail.com>
2021-07-14 18:53:47 -0300

    Criando arquivo para jogo forca

---
+++ forca.py
@@ -0,0 +1,6 @@
+def jogar():
+    print("**********************************")
+    print("***Bem vindo ao Jogo da Forca***")
+    print("**********************************")
+
+    print("\nFim do jogo")
